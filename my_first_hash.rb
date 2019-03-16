b65db81fc7180e91680d750171a484e57e3bac90
Kat B <katreesebarnes@gmail.com>
2019-03-16 18:03:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-def my_hash
+ldef my_hash
   
 my_hash = {"meat" => "beef", "veggie" => "asparagus"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
